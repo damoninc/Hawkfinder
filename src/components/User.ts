ec56b9b5a9b7251512e21e78f883bd3016da8d8b
MrGoldenSun <og2828@uncw.edu>
2023-02-17 02:18:19 -0500

    Added .ts run guide and accountSettings

--- src/components/User.ts
+++ src/components/User.ts
@@ -2,8 +2,14 @@ class User {
   private _username: string;
   private _password: string;
   private _email: string;
+  private _accountSettings: Map<string, string>;
 
   /**
+   * A User object which contains the user's information related to many core features.
+   *
+   * As of right now, accountSettings is an empty map that can be used to add
+   * an alternative email. We will create more methods that adds to the functionality
+   * of accountSettings.
    *
    * @param daEmail User's email
    * @param daPassword User's password
@@ -12,6 +18,15 @@ class User {
     this._email = daEmail;
     this._password = daPassword;
     this._username = this.createUsername();
+    this._accountSettings = new Map<string, string>();
+  }
+
+  /**
+   * Getter accountSettings
+   * @return {Map<string, string>}
+   */
+  public get accountSettings(): Map<string, string> {
+    return this._accountSettings;
   }
 
   /**
@@ -36,6 +51,14 @@ class User {
    */
   public get email(): string {
     return this._email;
+  }
+
+  /**
+   * Setter accountSettings
+   * @param {Map<string, string>} value
+   */
+  public set accountSettings(value: Map<string, string>) {
+    this._accountSettings = value;
   }
 
   /**
@@ -73,6 +96,25 @@ class User {
   }
 
   /**
+   * Adds alternative email to account settings
+   * @param alt the string that contains an alternative email
+   */
+  public addAltEmail(alt: string) {
+    this.accountSettings.set("alt-email", alt);
+  }
+
+  /**
+   * Will search if they have an alt email
+   * @returns alt email
+   */
+  public getAltEmail(): string {
+    if (this.accountSettings.has("alt-email")) {
+      return this.accountSettings.get("alt-email")!; // "!"" is used to tell typescript it won't be null
+    }
+    return "No Alternative Email";
+  }
+
+  /**
    *
    * @returns description of user, used for testing, should probably be depreciated afterwards
    */
@@ -83,7 +125,9 @@ class User {
       "\nEmail: " +
       this.email +
       "\nPassword: " +
-      this.password
+      this.password +
+      "\nAlternative Email: " +
+      this.getAltEmail()
     );
   }
 }
@@ -91,10 +135,17 @@ class User {
 // Sample code for initializing and printing array of User objects
 
 const testUsers: Array<User> = [
-  new User("og2828@uncw.edu", "123456789"),
-  new User("pio1681", "siuuuuuuuu"),
-  new User("dwi2359@uncw.edu", "Grugley is sexy ;)"),
+  new User("og2828@uncw.edu", "gamertime"),
+  new User("pio1681@uncw.edu", "siuuuuuuuu"),
+  new User("dwi2359@uncw.edu", "Grugley da master ;)"),
+  new User("rajebj@uncw.edu", "suppa hot FIYA")
 ];
+
+// These statements show off how you can add alt emails to Users.
+
+testUsers[0].addAltEmail("tabiogaming@gmail.com");
+testUsers[1].addAltEmail("elpatoorces@hotmail.com");
+testUsers[2].addAltEmail("gruggers@yahoo.com");
 
 console.log("Display Sample Users\n");
 for (let i = 0; i < testUsers.length; i++) {
