240b479a6db5ce1be4f78134e47eea410fa5718c
NicholasBrunsink <nicholasbrunsink@gmail.com>
2023-02-27 17:28:55 -0500

    visual changes and placed friendsystem in App.tsx

--- src/components/FriendSystem/FriendBox.tsx
+++ src/components/FriendSystem/FriendBox.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
 import "./FriendBox.css"
 import Profile from '../Profile';
-
-type FriendBoxProps = {
-    friend: Profile
-}
 
 function FriendBox(friend : Profile) { 
     let imgPath : string = "src/assets/images/" + friend.profilePicture;    // database call to grab image
@@ -19,21 +14,21 @@ function FriendBox(friend : Profile) {
     }
     return (
         <div className="container">
-                <div className='propic'>
-                    <img src={imgPath} width="100" height="100"></img>
-                </div>
-                <div className="content">
-                    <h3> {friend.firstName} {friend.lastName} </h3>
-                    <p>{interests}</p>
-                </div>   
-                <div className="buttons">
-                    <form action="/Messages">
-                        <button className="button">Messages</button>
-                    </form>   
-                    <form action="/Profile">
-                        <button className="button">Profile</button>
-                    </form>                  
-                    <button className='button' type="button">Remove</button>
+            <div className='propic'>
+                <img src={imgPath} width="100" height="100"></img>
+            </div>
+            <div className="content">
+                <h3> {friend.firstName} {friend.lastName} </h3>
+                <p>{interests}</p>
+            </div>   
+            <div className="buttons">
+                <form action="/Messages">
+                    <button className="button">Messages</button>
+                </form>   
+                <form action="/Profile">
+                    <button className="button">Profile</button>
+                </form>                  
+                <button className='button' type="button">Remove</button>
             </div>                                    
         </div>
     )
--- src/components/FriendSystem/FriendPage.tsx
+++ src/components/FriendSystem/FriendPage.tsx
@@ -1,56 +1,7 @@
-import React from 'react';
-import User from '../User';
 import Profile from '../Profile';
 import './FriendPage.css';
 import FriendBox from './FriendBox';
 import { sampleProfiles } from '../Profile';
-
-const userProfiles: Array<Profile> = [
-  new Profile("Damon","Incorvaia","dwi2359",
-    "Hello, my name is Damon Incorvaia. I am currently a Senior studying Computer Science! Other things about me: I am a musician of 12 years, I am a car enthusiast and I thorughly enjoy videogames and technology",
-    [
-      "Music",
-      "Videogames",
-      "Cars",
-      "Computers",
-      "Technology",
-      "Drums",
-      "Bass",
-      "Guitar",
-    ],
-    "profileimg.jpg",
-    "coverphoto.jpg",
-    new Date(2000, 6, 17)
-  ),
-  new Profile("Octavio","Galindo","og2828",
-    "My name is Octavio Galindo and I am a man of culture.",
-    ["Predatory Mobile Gaming", "Stale Bread", "Paint Chips", "Esoteric Art"],
-    "profileimg.jpg",
-    "coverphoto2.jpg",
-    new Date(1999, 12, 20)
-  ),
-  new Profile("Octavio","Galindo","og2828",
-  "My name is Octavio Galindo and I am a man of culture.",
-  ["Stale Bread", "Paint Chips", "Esoteric Art"],
-  "profileimg.jpg",
-  "coverphoto2.jpg",
-  new Date(1999, 12, 20)
-  ),
-  new Profile("Octavio","Galindo","og2828",
-  "My name is Octavio Galindo and I am a man of culture.",
-  ["Paint Chips", "Esoteric Art"],
-  "profileimg.jpg",
-  "coverphoto2.jpg",
-  new Date(1999, 12, 20)
-  ),
-  new Profile("Octavio","Galindo","og2828",
-  "My name is Octavio Galindo and I am a man of culture.",
-  ["Esoteric Art"],
-  "profileimg.jpg",
-  "coverphoto2.jpg",
-  new Date(1999, 12, 20)
-  ),
-];
 
 function FriendPage() {
   const dbCall : Array<Profile> = sampleProfiles // grab user friend's profiles using database call
