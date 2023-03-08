3e1e43852ac0f727df14c129da0566e3fa99c13a
NicholasBrunsink <nicholasbrunsink@gmail.com>
2023-03-08 13:50:59 -0500

    added dummy add/remove friend functionality and dummy navigation. Added documentation

--- src/components/FriendSystem/FriendBox.tsx
+++ src/components/FriendSystem/FriendBox.tsx
@@ -1,9 +1,17 @@
 import "./FriendBox.css"
 import Profile from '../Profile';
 
+/**
+ * Generates a HTML block that displays a user based on their Profile information
+ *      Currently displays the name and interests as well as having buttons to go to
+ *      their profile, messages, and removing them. 
+ * @param {Profile} friend - The user to display
+ * @return {*} - FriendBox HTML
+ */
 function FriendBox(friend : Profile) { 
     let imgPath : string = "src/assets/images/" + friend.profilePicture;    // database call to grab image
 
+    // Generating string of interests, cutting off after ~25 characters 
     let interests : string = friend.interests[0];
     for(let i=1; i<friend.interests.length; i++){
         if (interests.length > 25) {
@@ -23,10 +31,10 @@ function FriendBox(friend : Profile) {
             </div>   
             <div className="buttons">
                 <form action="/Messages">
-                    <button className="button">Messages</button>
+                    <button className="button" onClick={goToMessages}>Messages</button>
                 </form>   
                 <form action="/Profile">
-                    <button className="button">Profile</button>
+                    <button className="button" onClick={goToProfile}>Profile</button>
                 </form>                  
                 <button className='button' type="button" onClick={removeFriend}>Remove</button>
             </div>                                    
@@ -35,7 +43,18 @@ function FriendBox(friend : Profile) {
 }
 
 function removeFriend() {
+    // Dummy functionality for removing a friend. Currently only sends an alert
     alert('Friend Removed')
+}
+
+function goToMessages() {
+    // Dummy function for navigating to messages
+    alert('Cannot go to Messages')
+}
+
+function goToProfile() {
+    // Dummy function for navigating to profile
+    alert('Cannot go to Profile')
 }
 
 export default FriendBox;
--- src/components/FriendSystem/FriendPage.tsx
+++ src/components/FriendSystem/FriendPage.tsx
@@ -3,16 +3,27 @@ import './FriendPage.css';
 import FriendBox from './FriendBox';
 import { sampleProfiles } from '../Profile';
 
+/**
+ * Generates a HTML block that displays a user's friend list by creating
+ *    a FriendBox for each friend in their friend list.
+ * Shows all friends or a "no friends" message is applicable. 
+ *
+ * @return {*} - FriendPage HTML
+ */
 function FriendPage() {
+  // FriendPage component which displays a friends list based on the currently active user
+  //
+  // returns: HTML for displaying the users insides of the current user's friend's list.
+  //          If the user does not have any friends, it returns an empty list.
+
   const dbCall : Array<Profile> = sampleProfiles // grab user friend's profiles using database call
-  const BarStyle = {width:"20rem",background:"#F0F0F0", border:"none", padding:"0.5rem"};
 
   return (
     <div className='friendPage'>
-        <h1>Friends List</h1>
+        <h1>Friends List</h1> 
         <div>
           <input type="text" placeholder="Username" />
-          <button>Add Friend</button>
+          <button type="button" onClick={addFriend}>Remove</button>
         </div>
         {checkNullList(dbCall)}
     </div>
@@ -20,6 +31,7 @@ function FriendPage() {
 }
 
 function checkNullList(friends : Profile[]) {
+  // Returns a list of FriendBox if the user's friends list is not empty
   if (friends.length == 0) {
     return (
           <div>
@@ -37,8 +49,9 @@ function checkNullList(friends : Profile[]) {
   }
 }
 
-function addFriend(friendUser : string) {
-  alert('Adding Friend ' + friendUser)
+function addFriend() {
+  // dummy function for adding a friend, no friend information is transfered
+  alert('Adding Friend')
 }
 
 export default FriendPage
