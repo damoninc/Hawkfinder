3017cb447213f20459c9bf8e8ab1ff7d902e120f
johnny <jbejar2001@yahoo.com>
2023-03-19 22:38:07 -0400

    moved firebase configuration into its own folder

--- src/App.tsx
+++ src/App.tsx
@@ -25,30 +25,6 @@ const posts = SAMPLE_POSTS.map((post) => {
   );
 });
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDNM-tQPGKeAblOqQPqZnBKwRnpGxOvc0Q",
-  authDomain: "csc-450-project.firebaseapp.com",
-  projectId: "csc-450-project",
-  storageBucket: "csc-450-project.appspot.com",
-  messagingSenderId: "877784266308",
-  appId: "1:877784266308:web:31ab02901a000bc93fae11",
-  measurementId: "G-RVEP3DC122",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
 function App() {
   return (
     <div className="app">
