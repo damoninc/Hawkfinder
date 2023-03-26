ed1193a70bf5456e1050ac2e503c8bc4ea40cf67
johnny <jbejar2001@yahoo.com>
2023-03-26 16:03:14 -0400

    Added loading progress while webapp fetches from database

--- src/components/Forum/Forum.tsx
+++ src/components/Forum/Forum.tsx
@@ -5,6 +5,7 @@ import Post from "../../data/Post";
 import PostView from "../Post/PostView";
 import ForumPost from "./ForumPost";
 import { Modal, Box } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import "../../styles/forum.css";
 
 function Forum() {
@@ -13,8 +14,10 @@ function Forum() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [postIndex, setPostIndex] = useState(0);
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const fetchPosts = async () => {
+    setLoading(true);
     await getDocs(collection(db, "Posts")).then((querySnapshot) => {
       const tempPosts: any = querySnapshot.docs.map((doc) => {
         console.log("DB CALL");
@@ -28,6 +31,7 @@ function Forum() {
         );
       });
       setPosts(tempPosts);
+      setLoading(false);
     });
   };
 
@@ -43,58 +47,67 @@ function Forum() {
 
   return (
     <div className="forum-container">
-      {posts.map((post: Post, index) => {
-        console.log("NEW POST COMPONENT RETURNED");
-        return (
-          <div
-            key={index}
-            onClick={() => handleOpen(index)}
-            className="post-handler"
-          >
-            <ForumPost
-              // key={post.postID}
-              postDate={post.postDate}
-              description={post.description}
-              interest={post.interest}
-              imageURL={post.imageURL}
-              ratings={post.ratings}
-              rating={post.calculateRating()}
-            />
-          </div>
-        );
-      })}
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <Box
-          sx={{
-            display: "flex",
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 0.6,
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          {/* This expression is required or else this code will somehow
-              run before the db call is made and return an typeerror */}
-          {posts.length > 0 ? (
-            <PostView
-              key={postIndex}
-              postDate={posts[postIndex].postDate}
-              description={posts[postIndex].description}
-              interest={posts[postIndex].interest}
-              imageURL={posts[postIndex].imageURL}
-              ratings={posts[postIndex].ratings}
-              rating={posts[postIndex].calculateRating()}
-            />
-          ) : (
-            <></>
-          )}
-        </Box>
-      </Modal>
+      {!loading ? (
+        <>
+          {posts.map((post: Post, index) => {
+            console.log("NEW POST COMPONENT RETURNED");
+            return (
+              <div
+                key={index}
+                onClick={() => handleOpen(index)}
+                className="post-handler"
+              >
+                <ForumPost
+                  // key={post.postID}
+                  postDate={post.postDate}
+                  description={post.description}
+                  interest={post.interest}
+                  imageURL={post.imageURL}
+                  ratings={post.ratings}
+                  rating={post.calculateRating()}
+                />
+              </div>
+            );
+          })}
+          <Modal open={open} onClose={() => setOpen(false)}>
+            <Box
+              sx={{
+                display: "flex",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 0.6,
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              {/* This expression is required or else this code will somehow
+                run before the db call is made and return an typeerror */}
+              {posts.length > 0 ? (
+                <PostView
+                  key={postIndex}
+                  postDate={posts[postIndex].postDate}
+                  description={posts[postIndex].description}
+                  interest={posts[postIndex].interest}
+                  imageURL={posts[postIndex].imageURL}
+                  ratings={posts[postIndex].ratings}
+                  rating={posts[postIndex].calculateRating()}
+                />
+              ) : (
+                <></>
+              )}
+            </Box>
+          </Modal>
+        </>
+      ) : (
+        <>
+          <h2>Loading Forum...</h2>
+          <CircularProgress />
+        </>
+      )}
     </div>
   );
 }
