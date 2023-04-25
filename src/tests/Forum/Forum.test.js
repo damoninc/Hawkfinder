48407a0c7958451e4db545ec3baddc2acb57e8d3
John Bejar <jbejar2001@yahoo.com>
2023-04-24 23:26:13 -0400

    added tests for creating posts and rating posts

--- src/tests/Forum/Forum.test.js
+++ src/tests/Forum/Forum.test.js
@@ -1,14 +1,31 @@
+const { url } = require("inspector");
+const {By, Key, Builder} = require("selenium-webdriver");
+let assert = require('assert');
+
 // Require selenium webdriver
 let webdriver = require("selenium-webdriver");
   
 // Require webdriver for chrome
 // browser called chromedriver
 require("chromedriver");
-// require("geckodriver");
-  
-// Build new window of chrome
-let driver = new webdriver.Builder()
-    .forBrowser("chrome").build();
-  
-// Open geeksforgeeks using get method
-driver.get("http://localhost:5173");
+
+async function test() {
+    // Build new window of chrome
+    const driver = await new webdriver.Builder().forBrowser("chrome").build();
+    
+    await driver.get("http://localhost:5173");
+    setTimeout(() => {
+        driver.findElement(By.id("email")).sendKeys("jbejar2001@uncw.edu");
+        driver.findElement(By.id("password")).sendKeys("johnny123", Key.RETURN);
+        setTimeout(async () => {
+            let alert = driver.switchTo().alert();
+            alert.accept();
+            const url = await driver.getCurrentUrl();
+
+            assert(url == "https://localhost:5173/components/Forum", "Forum has loaded successfully");
+        }, 1000)
+    }, 1000)
+
+}
+
+test()
